e9b3fccd2fbc255271dc4781b723ea573faef702
SC Khoo <sckhoo@gmail.com>
2024-03-17 10:40:32 +0800

    sushi

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/", tags=["root"])
 async def root():
-    return {"message": "Welcome to CM CI/CD Demo 17March 1035am"}
+    return {"message": "Welcome to CM CI/CD Demo 17March 1035am - I am eating sushi"}
